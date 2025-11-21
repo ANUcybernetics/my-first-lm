bb8aaba06cc28df722d3b83fda53de0b1f1a1228
Ben Swift <ben@benswift.me>
2025-11-21 21:47:23 +1100

    Fix release script to use handouts Makefile

--- scripts/release.sh
+++ scripts/release.sh
@@ -28,7 +28,8 @@ jq ".version = \"$VERSION\"" website/package.json > website/package.json.tmp && 
 jq ". + {version: \"$VERSION\"}" .zenodo.json > .zenodo.json.tmp && mv .zenodo.json.tmp .zenodo.json
 
 echo "Regenerating modules.pdf with new version..."
-cd website && npm run ensure-pdfs && cd ..
+cd handouts && make modules && cd ..
+cp handouts/out/modules.pdf website/src/assets/pdfs/
 
 echo "Committing version bump..."
 git add cli/Cargo.toml website/package.json .zenodo.json website/src/assets/pdfs/modules.pdf
